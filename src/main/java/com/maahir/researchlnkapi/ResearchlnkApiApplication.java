1bcc0078361727a665ff401cfdf178e8911097cc
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-20 11:33:50 -0400

    first commits

---
+++ src/main/java/com/maahir/researchlnkapi/ResearchlnkApiApplication.java
@@ -0,0 +1,13 @@
+package com.maahir.researchlnkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchlnkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchlnkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maahir/researchlnkapi/ResearchlnkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maahir.researchlnkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchlnkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
