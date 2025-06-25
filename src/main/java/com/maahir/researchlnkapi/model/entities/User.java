1fa1edfc707a364fb42537a297359aec7d1f9bc8
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-25 15:29:21 -0400

    folder structure + user entity complete

---
+++ src/main/java/com/maahir/researchlnkapi/model/entities/User.java
@@ -0,0 +1,41 @@
+package com.maahir.researchlnkapi.model.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "orcidId")
+    private String orcidId;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
