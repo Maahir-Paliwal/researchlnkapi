85eb820359d738dc7fa56cd719b37f457ead8985
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-26 17:11:46 -0400

    first repository : user repository

---
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.maahir.researchlnkapi.model.repositories;
+
+import com.maahir.researchlnkapi.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
