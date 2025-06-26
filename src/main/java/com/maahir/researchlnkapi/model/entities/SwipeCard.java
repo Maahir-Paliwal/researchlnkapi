517f0f43a05e33867ee42bfcbf2bf5fedb9f6080
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-26 11:36:32 -0400

    SwipeCard entity

---
+++ src/main/java/com/maahir/researchlnkapi/model/entities/SwipeCard.java
@@ -0,0 +1,33 @@
+package com.maahir.researchlnkapi.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name ="swipeCards")
+public class SwipeCard {
+    @Id
+    @Column(name ="id")
+    private Long id;
+
+    @Column(name ="name", nullable = false)
+    private String name;
+
+    @Column(name = "position", nullable = false)
+    private String position;
+
+    @Column(name ="description", nullable = false)
+    private String description;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id")
+    @MapsId
+    private Profile profile;
+
+}
