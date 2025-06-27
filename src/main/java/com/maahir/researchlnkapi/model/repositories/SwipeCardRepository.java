d434c771275b2d9c8a8c2726dc8a235355502f5d
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-27 13:07:30 -0400

    swipe card repository

---
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/SwipeCardRepository.java
@@ -0,0 +1,7 @@
+package com.maahir.researchlnkapi.model.repositories;
+
+import com.maahir.researchlnkapi.model.entities.SwipeCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwipeCardRepository extends JpaRepository<SwipeCard, Long> {
+}
