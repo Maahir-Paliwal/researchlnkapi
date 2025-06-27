9e733f8f667ea0054818626d3fb205aa44b6b6d2
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-06-27 13:10:07 -0400

    Relevant Experience Repo

---
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/RelevantExperienceRepository.java
@@ -0,0 +1,7 @@
+package com.maahir.researchlnkapi.model.repositories;
+
+import com.maahir.researchlnkapi.model.entities.RelevantExperience;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelevantExperienceRepository extends JpaRepository<RelevantExperience, Long> {
+}
