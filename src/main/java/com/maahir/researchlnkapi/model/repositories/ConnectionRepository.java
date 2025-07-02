156cc54cfaf312a86af98878fb9358322f5f0b51
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-07-02 10:29:27 -0400

    Jpa repo for Connection

---
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/ConnectionRepository.java
@@ -0,0 +1,8 @@
+package com.maahir.researchlnkapi.model.repositories;
+
+import com.maahir.researchlnkapi.model.entities.Connection;
+import com.maahir.researchlnkapi.model.keys.ConnectionId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConnectionRepository extends JpaRepository<Connection, ConnectionId> {
+}
