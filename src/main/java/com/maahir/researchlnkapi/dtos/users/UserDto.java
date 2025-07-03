8be62affd40146706f28ec435c2abff397ddc9fd
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-07-03 10:52:11 -0400

    changing userDto

--- src/main/java/com/maahir/researchlnkapi/dtos/users/UserDto.java
+++ src/main/java/com/maahir/researchlnkapi/dtos/users/UserDto.java
@@ -3,13 +3,12 @@ package com.maahir.researchlnkapi.dtos.users;
 import lombok.Data;
 import java.time.LocalDateTime;
 
+//backend -> frontend
+
 @Data
 public class UserDto {
     private Long id;
-    private String name;
     private String email;
-    private String password;
-    private String orcidId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
