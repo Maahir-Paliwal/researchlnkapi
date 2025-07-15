2f19dc208e18d7363a5def519af91a8484398269
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-07-15 12:38:11 -0400

    userMapper complete

--- src/main/java/com/maahir/researchlnkapi/mappers/UserMapper.java
+++ src/main/java/com/maahir/researchlnkapi/mappers/UserMapper.java
@@ -2,9 +2,11 @@ package com.maahir.researchlnkapi.mappers;
 
 
 import com.maahir.researchlnkapi.dtos.users.RegisterUserRequest;
+import com.maahir.researchlnkapi.dtos.users.UpdateUserRequest;
 import com.maahir.researchlnkapi.dtos.users.UserDto;
 import com.maahir.researchlnkapi.model.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -12,5 +14,7 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     User toEntity(RegisterUserRequest request);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
 
 }
