719b09c454ba2e5aebbb11723e840aebe8e5f4ce
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-07-16 10:46:21 -0400

    userRepo to include exists by email

--- src/main/java/com/maahir/researchlnkapi/services/UserService.java
+++ src/main/java/com/maahir/researchlnkapi/services/UserService.java
@@ -1,5 +1,24 @@
 package com.maahir.researchlnkapi.services;
 
+
+import com.maahir.researchlnkapi.mappers.UserMapper;
+import com.maahir.researchlnkapi.model.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
 }
