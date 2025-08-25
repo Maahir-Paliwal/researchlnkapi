8ddb64a8a76d6ea4b0e3d1d0c24c051be9d6e78d
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-08-25 15:24:18 -0400

    updating query in profileExperience

--- src/main/java/com/maahir/researchlnkapi/model/repositories/ProfileExperienceRepository.java
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/ProfileExperienceRepository.java
@@ -2,6 +2,29 @@ package com.maahir.researchlnkapi.model.repositories;
 
 import com.maahir.researchlnkapi.model.entities.ProfileExperience;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProfileExperienceRepository extends JpaRepository<ProfileExperience, Long> {
+
+    @Query("""
+        select pe from ProfileExperience pe
+        WHERE pe.profile.id =: profileId
+        ORDER BY
+                CASE WHEN pe.endAt IS NULL THEN 1 ELSE 0 END DESC,
+                COALESCE(pe.endAt, pe.startAt) DESC, 
+                pe.startAt DESC
+    """)
+    List<ProfileExperience> findOrderedByProfileId(@Param("profileId") Long profileId);
+
+    long countByProfile_Id(Long profileId);
+
+    Optional<ProfileExperience> findByIdAndProfile_Id(Long id, Long profileId);
+
+    long deleteByIdAndProfile_Id(Long id, Long profileId);
+
+    boolean existsByIdAndProfile_Id(Long id, Long profileId);
 }
