61d1a0836025f7c6726ef62aa603b64d196b91fb
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2025-08-26 15:41:37 -0400

    profile repo

--- src/main/java/com/maahir/researchlnkapi/model/repositories/ProfileRepository.java
+++ src/main/java/com/maahir/researchlnkapi/model/repositories/ProfileRepository.java
@@ -3,5 +3,8 @@ package com.maahir.researchlnkapi.model.repositories;
 import com.maahir.researchlnkapi.model.entities.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByPublicId(String publicId);
 }
